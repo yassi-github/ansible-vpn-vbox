65156267e497687efc91b241fe922bfbfb74104a
yassi-github <s19t312@stu.kagawa-u.ac.jp>
2021-04-05 02:57:59 +0900

    add vagrant role

--- shellscript/apt-key-add.sh
+++ shellscript/apt-key-add.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 keyURI=$1
-keyName=`echo $keyURI | awk -F'/' '{print $(NF)}'`
+
+if [ $# -gt 1 ]; then
+    keyName=$2
+else
+    keyName=`echo $keyURI | awk -F'/' '{print $(NF)}'`
+fi
+
 curl $keyURI | gpg --dearmor > /usr/share/keyrings/$keyName
