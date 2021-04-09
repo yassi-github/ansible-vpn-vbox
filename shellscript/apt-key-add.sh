ab71cc760399b23599b3a11b36012c1aceca7064
yassi-github <s19t312@stu.kagawa-u.ac.jp>
2021-04-10 07:29:09 +0900

    modify apt-key-add.sh catch curl error and try using wget (uri param that includes '&' should enclosed by quotations, but single quotation causes error at the curl command...)

--- shellscript/apt-key-add.sh
+++ shellscript/apt-key-add.sh
@@ -7,4 +7,6 @@ else
     keyName=`echo $keyURI | awk -F'/' '{print $(NF)}'`
 fi
 
+trap 'wget -O- $keyURI | gpg --dearmor > /usr/share/keyrings/$keyName' ERR
+
 curl $keyURI | gpg --dearmor > /usr/share/keyrings/$keyName
