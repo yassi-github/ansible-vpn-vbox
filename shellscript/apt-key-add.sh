a75d5866c6889b34621826bfd2654a8a9c25a236
yassi-github <s19t312@stu.kagawa-u.ac.jp>
2021-07-17 22:02:15 +0900

    add git

--- shellscript/apt-key-add.sh
+++ shellscript/apt-key-add.sh
@@ -7,6 +7,12 @@ else
     keyName=`echo $keyURI | awk -F'/' '{print $(NF)}'`
 fi
 
+# 拡張子がない場合 or 拡張子がgpgでない場合は、gpg拡張子を追加する。
+if [ $(echo $keyName | awk -F'.' '{print $(NF)}') = $keyName ] \
+    || [ $(echo $keyName | awk -F'.' '{print $(NF)}') != 'gpg' ]; then
+        keyName=${keyName}.gpg
+fi
+
 trap 'wget -O- $keyURI | gpg --dearmor > /usr/share/keyrings/$keyName' ERR
 
 curl $keyURI | gpg --dearmor > /usr/share/keyrings/$keyName
